30def6281627d6b31757d479c00051d74f0fb99c
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2025-07-29 19:30:29 -0300

    add utils

---
+++ src/utils/conts.ts
@@ -0,0 +1,19 @@
+// Branding de Coca-Cola
+const brandingData = {
+  companyName: "Coca-Cola",
+  brandColor: "#E60012",
+  logoUrl: "/CocaColalogo.png",
+  motive: "¡Destapa la Felicidad en Equipo!",
+  color: 'red'
+};
+
+// Branding de YPF
+// const brandingData = {
+//   companyName: "YPF",
+//   brandColor: "#0451DD",
+//   logoUrl: "/ypf_logoazul.svg",
+//   motive: "Transformamos vidas a través de la energía",
+//   color: 'blue'
+// }
+
+export default brandingData;
