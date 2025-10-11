455716995c306e14839e94c2cc688036a11fd8bd
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2025-10-11 18:02:21 -0300

    Change Banner aspect ratio

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="aspect-[21/3] bg-black/20 backdrop-blur-sm">
+    <div className="aspect-[21/6] bg-black/20 backdrop-blur-sm">
         <video
           className="w-full h-full object-cover"
           src="/videos/banner.mp4"
